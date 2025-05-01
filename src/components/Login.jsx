5089adfcc9339fae48bb16e4ff6dcb3b0a6c96a7
Prakash <prash8264@gmail.com>
2025-05-01 13:46:01 +0530

    difine the userData

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,35 @@
-import React,{useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {login as authLogin} from '../store/authSlice'
-import {Button, Input, Logo} from './index'
-import { useDispatch } from 'react-redux'
-import authService from '../appwrite/auth'
-import { set, useForm } from 'react-hook-form'
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import authService from "../appwrite/auth"
+import {useForm} from "react-hook-form"
 
 function Login() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const {register, handleSubmit} = useForm();
-    const  [error, setError] = useState("")
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
 
-    const login=  async(data) => {
+    const login = async(data) => {
         setError("")
         try {
             const session = await authService.login(data)
-            if(useData) dispatch(authLogin(useData));
-            navigate("/")
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
         } catch (error) {
             setError(error.message)
         }
     }
+
   return (
-    <div className='flex items-center justify-center w-full'>
+    <div
+    className='flex items-center justify-center w-full'
+    >
         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
         <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
@@ -41,40 +47,35 @@ function Login() {
                     </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)}
-        className='mt-8'>
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
             <div className='space-y-5'>
-              <Input 
-              label = "Email: "
-              placeholder = "Enter your email"
-              type =  "email"
-              {...register("email",
-                {
+                <Input
+                label="Email: "
+                placeholder="Enter your email"
+                type="email"
+                {...register("email", {
                     required: true,
                     validate: {
                         matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
                         "Email address must be a valid address",
                     }
                 })}
-              />
-              <Input 
-              label = "password: "
-              placeholder = "Enter the password"
-              type = "password"
-              {...register("password",
-                {
+                />
+                <Input
+                label="Password: "
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
                     required: true,
-                }
-              )}
-              />
-              <Button
-              type = "submit"
-              className='w-full'
-              >Sign in</Button>
+                })}
+                />
+                <Button
+                type="submit"
+                className="w-full"
+                >Sign in</Button>
             </div>
         </form>
         </div>
-      
     </div>
   )
 }
