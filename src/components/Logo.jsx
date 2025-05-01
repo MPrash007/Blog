f576f5ded4ce4526542e0411fada57cf5c39311a
Prakash <prash8264@gmail.com>
2025-05-01 09:32:48 +0530

    Add logo image to Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/Image.png'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src={logoImage} alt="MegaBlog Logo" style={{ width }} />
+      <img src={logo} alt="MegaBlog Logo" style={{ width }} />
     </div>
   )
 }
